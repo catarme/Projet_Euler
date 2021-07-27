0584e8e537eefe9a32ad182d56920c081af7cf60
catarme <71098898+catarme@users.noreply.github.com>
2021-07-28 01:10:35 +0200

    Update Special_Pythagorean_triplet.py

--- Problems Archives/Special_Pythagorean_triplet.py
+++ Problems Archives/Special_Pythagorean_triplet.py
@@ -2,10 +2,8 @@
 def main():
     """
     A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-
     a2 + b2 = c2
     For example, 32 + 42 = 9 + 16 = 25 = 52.
-
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product abc.
     """
@@ -15,12 +13,6 @@ def main():
                 if a + b + c == 1000 and a**2 + b**2 == c**2:
                     print(a, b, c)
                     print(a * b * c)
-    
-    # for i in range(1000):
-    #     for x in range(1000):
-    #         for c in range(1000):
-    #             if i + x + c == 1000:
-    #                 return i, x, c
 
 if __name__ == '__main__':
     print(main())
