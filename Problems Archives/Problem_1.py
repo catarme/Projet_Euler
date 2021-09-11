732ab24308019026b41b1d729a408e71282f518e
catarme <celeste.francier@protonmail.com>
2021-09-11 21:45:10 +0200

    update 1

--- Problems Archives/Problem_1.py
+++ Problems Archives/Problem_1.py
@@ -1,15 +1,4 @@
 
-def Multiples_of_3_or_5():
-    L = []
-    for i in range(1000):
-        if i % 3 == 0 or i % 5 == 0:
-            L.append(i)
-    result = 0
-    for i in L:
-        result+= i
-    
-    return result
-
 if __name__ == "__main__":
     L = []
     for i in range(1000):
